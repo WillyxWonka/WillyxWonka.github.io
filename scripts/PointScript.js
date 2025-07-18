ce3c8267bbb3a8e0b40848a75436c9750b3ab973
KJ <kalif.edu@gmail.com>
2025-07-17 18:54:38 -0700

    Update
    update CSS
    updated text inconsistency - word decoder.

--- scripts/PointScript.js
+++ scripts/PointScript.js
@@ -13,28 +13,28 @@ export function CheckPoints(Points, dataSource)
             {
                 FreeLetters = 7
                 Difficulty.innerText = "Difficulty: Very Easy"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
             if(Points >= 0)
             {
                 FreeLetters = 5;
                 Difficulty.innerText = "Difficulty: Easy"
                 Difficulty.style.color = "#008000"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
             if(Points >= 3)
             {
                 FreeLetters = 4;
-                Difficulty.innerText = "Difficulty: Moderate"
+                Difficulty.innerText = "Difficulty: Medium"
                 Difficulty.style.color = "#8B8000"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
             if(Points >=7)
             {
                 FreeLetters = 3
-                Difficulty.innerText = "Difficulty: Challenging"
+                Difficulty.innerText = "Difficulty: Difficult"
                 Difficulty.style.color = "#ff0000"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
             if(Points >=10)
             {
@@ -43,7 +43,7 @@ export function CheckPoints(Points, dataSource)
                 Difficulty.style.color = "#9090ff"
                 document.getElementById("header").innerText = ("CONGRATULATIONS! You beat the Word Decoder alpha. All word difficulties are now possible, Good Luck!")
                 Difficulty.innerText = "Difficulty: All"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
             //return(FreeLetters)
         break;
@@ -51,27 +51,27 @@ export function CheckPoints(Points, dataSource)
         if(Points >= 0)
         {
             FreeLetters = 5;
-            Difficulty.innerText = "Difficulty: All Easy"
+            Difficulty.innerText = "Difficulty: Easy"
             Difficulty.style.color = "#008000"
-            FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+            FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
         }
         break;
         case "Medium":
             if(Points >= 0)
             {
                 FreeLetters = 4;
-                Difficulty.innerText = "Difficulty: All Moderate"
+                Difficulty.innerText = "Difficulty: Medium"
                 Difficulty.style.color = "#8B8000"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
         break;
         case "Difficult":
             if(Points >= 0)
             {
                 FreeLetters = 3;
-                Difficulty.innerText = "Difficulty: All Difficult"
+                Difficulty.innerText = "Difficulty: Difficult"
                 Difficulty.style.color = "#ff0000"
-                FreeLetterIndicator.innerText = "Free Letters " + FreeLetters
+                FreeLetterIndicator.innerText = "Free Letters: " + FreeLetters
             }
         break;
     }
